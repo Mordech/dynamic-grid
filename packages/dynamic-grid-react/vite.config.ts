4b301e7d700439e6f7584250e03c0872381789e4
Elad Mordechai Mizrahi <elad@mizrahi.cc>
2025-03-08 10:51:07 +0200

    chore: upgrade dependencies

--- packages/dynamic-grid-react/vite.config.ts
+++ packages/dynamic-grid-react/vite.config.ts
@@ -5,23 +5,22 @@ import dts from 'vite-plugin-dts';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 export default defineConfig({
-  plugins: [dts({ exclude: ['**/*.test.tsx'] }), react(), libInjectCss()],
-  css: {
-    modules: {
-      localsConvention: 'camelCaseOnly',
-    },
-  },
+  plugins: [
+    dts({
+      exclude: ['**/*.test.tsx', './lib/App.tsx', './lib/main.tsx'],
+      insertTypesEntry: true,
+    }),
+    react(),
+    libInjectCss(),
+  ],
+  css: { modules: { localsConvention: 'camelCaseOnly' } },
   build: {
     outDir: 'dist',
     lib: {
       formats: ['es'],
       name: 'dynamic-grid-react',
-      entry: {
-        index: 'lib/index.ts',
-      },
+      entry: { index: 'lib/index.ts' },
     },
-    rollupOptions: {
-      external: ['react', 'react/jsx-runtime'],
-    },
+    rollupOptions: { external: ['react', 'react/jsx-runtime'] },
   },
 });
